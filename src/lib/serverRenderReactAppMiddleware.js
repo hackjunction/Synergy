443d6036cc3fbe585b27fae71b995b4226981dd9
Tomi Hiltunen <tomi@innovaniac.com>
2017-06-21 19:35:43 +0300

    add HTML template for server rendered app

--- src/lib/serverRenderReactAppMiddleware.js
+++ src/lib/serverRenderReactAppMiddleware.js
@@ -3,11 +3,17 @@ import ReactDOMServer from 'react-dom/server';
 
 import TempApp from 'src/TempApp';
 
+// TODO: use templating engine and move HTML template to an external file
+const renderHTML = (appHtml) => `<html>
+  <body>
+    <div id="app">${appHtml}</div>
+  </body>
+</html>`;
+
 const serverRenderReactAppMiddleware = (req, res) => {
-  const renderedHTML = ReactDOMServer.renderToString(
-    <TempApp />
-  );
-  res.send(renderedHTML);
+  const appHtml = ReactDOMServer.renderToString(<TempApp />);
+  const completeHTML = renderHTML(appHtml);
+  res.send(completeHTML);
 };
 
 export default serverRenderReactAppMiddleware;
