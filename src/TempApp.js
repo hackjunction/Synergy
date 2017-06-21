2ec966ba21ecb8475b237c430bb62e53e860e1fd
Tomi Hiltunen <tomi@innovaniac.com>
2017-06-21 19:12:59 +0300

    add initial server rendering for React app

---
+++ src/TempApp.js
@@ -0,0 +1,12 @@
+/*
+ * THIS IS TEMPORARY
+ * Move to an appropriate location and
+ * setup the actual root app component
+ */
+import React from 'react';
+
+const TempApp = () => (
+  <h1>Hellou!</h1>
+);
+
+export default TempApp;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import { argv } from 'yargs';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import TempApp from 'src/TempApp';
 
 const port = argv.port || process.env.PORT || 3000;
 const app = express();
 
 app.all("*", (req, res) => {
-  res.status(404).send("Not found");
+  const renderedHTML = ReactDOMServer.renderToString(
+    <TempApp />
+  );
+  res.send(renderedHTML);
 });
 
 app.listen(port);
