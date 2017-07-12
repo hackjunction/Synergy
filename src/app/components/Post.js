058243085e9135c1a5063eeed05ad770f6a87107
Tomi Hiltunen <tomi@innovaniac.com>
2017-07-12 18:43:00 +0300

    fix amgibuous export/import

--- src/app/components/Post.js
+++ src/app/components/Post.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 import { PostList } from "./common/PostList";
 
-export class Post extends Component {
+class Post extends Component {
   constructor(props) {
     super(props);
   }
