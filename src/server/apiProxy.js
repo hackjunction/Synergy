3779e66bd9b868fda4c2a125290baa96b67fa412
Tomi Hiltunen <tomi@innovaniac.com>
2017-07-19 18:52:52 +0300

    method for getting events from hackjunction Facebook page

--- src/server/apiProxy.js
+++ src/server/apiProxy.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as wordpressApi from './lib/wordpressApi';
 import facebookClient from './lib/facebookClient';
 
+const FACEBOOK_PAGE = 'hackjunction';
+
 const apiRouter = express.Router();
 
 apiRouter.get('/posts', (req, res) => {
@@ -13,9 +15,29 @@ apiRouter.get('/posts', (req, res) => {
 });
 
 apiRouter.get('/fb/posts', (req, res) => {
-  facebookClient.api('/hackjunction', (fbRes) => {
+  facebookClient.api(`/${FACEBOOK_PAGE}`, (fbRes) => {
     res.json(fbRes);
   });
+});
+
+apiRouter.get('/fb/events', (req, res) => {
+  facebookClient.api(
+    `/${FACEBOOK_PAGE}/events`,
+    {
+      fields: [
+        'id',
+        'name',
+        'description',
+        'cover',
+        'place',
+        'start_time',
+        'end_time',
+      ],
+    },
+    (fbRes) => {
+      res.json(fbRes.data);
+    }
+  );
 });
 
 export default apiRouter;
