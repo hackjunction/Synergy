ba6e4e91e000d34e43ed6e42fa683b52e222dfe1
Ujjwal <ujjwal.mairh@gmail.com>
2017-08-14 11:20:40 +0300

    landing page footer

--- src/app/components/landing/Footer.js
+++ src/app/components/landing/Footer.js
@@ -3,18 +3,18 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import './landing.scss';
 
 const Footer = () =>
-  <Grid>
+  <Grid fluid>
     <Row start="xs" className="junction_footer">
-      <Col lg={3}>
+      <Col xsOffset={1} lg={2}>
         <h2>Event</h2>
       </Col>
-      <Col lg={3}>
+      <Col xsOffset={1} lg={2}>
         <h2>For Media</h2>
       </Col>
-      <Col lg={3}>
+      <Col xsOffset={1} lg={2}>
         <h2>Social</h2>
       </Col>
-      <Col lg={3}>
+      <Col xsOffset={1} lg={2}>
         <h2>Community</h2>
       </Col>
     </Row>
