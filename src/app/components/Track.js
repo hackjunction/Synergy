af5b2041844618125f91d99cfc6abfebb1a5691e
Henrik Aarnio <hjaarnio@gmail.com>
2017-08-20 23:18:06 +0300

    adjust track styles more

--- src/app/components/Track.js
+++ src/app/components/Track.js
@@ -25,18 +25,32 @@ class Track extends Component {
     };
 
     return (
-      <Grid fluid>
+      <div>
+
+      <Grid className={styles.hero} fluid>
+        <Row center="xs sm md">
+          <Col>
+            <img
+              className="junction_logo"
+              src="https://staging.hackjunction.com/wp-content/uploads/2017/08/junction_logo-1.png"
+            />
+          </Col>
+        </Row>
+
         <Row center="xs sm md" style={style} className={styles.track_header}>
           <Col className={styles.track_name} xs={12} sm={12} md={12}>
             <h1>{track.title}</h1>
           </Col>
         </Row>
+      </Grid>
+      <Grid fluid>
         <Row center="xs sm md">
           <Col className="track_content" xs={12} sm={9} md={9}>
             <div dangerouslySetInnerHTML={{__html: track.content}}></div>
           </Col>
         </Row>
       </Grid>
+      </div>
     );
   }
 }
