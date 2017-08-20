bc648a41a18dc75746c3f81de82a633c894465d6
Angelos <angelos.arnis@gmail.com>
2017-08-20 19:53:35 +0300

    fixing partners texts

--- src/app/components/landing/Partners.js
+++ src/app/components/landing/Partners.js
@@ -14,12 +14,12 @@ const Partners = () =>
         </h1>
       </Col>
       <Col xsOffset={1} xs={6}>
-        A unique opportunity to the front row of Engineering, Design and
-        Business Development, happens once a year before Slush. You would not
-        want to miss out. Contact our Head of Partnerships, Mikko for more info
-        mikko.kuusisto-at-hackjunction.com
+        <p>A unique opportunity to the front row of Engineering, Design and
+        Business Development, happens once a year before Slush. <br /><br />You would not
+        want to miss out. Contact our Head of Partnerships, Mikko for more info:
+        <br />mikko.kuusisto-at-hackjunction.com</p>
       </Col>
-      <Col xsOffset={2} xs={9}>
+      <Col xs={12}>
         <Col xs={4}>
           <img src="https://staging.hackjunction.com/wp-content/uploads/2017/08/elisa.png" className="responsive" />
         </Col>
