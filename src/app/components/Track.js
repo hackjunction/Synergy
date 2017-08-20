6ccaf7d13d7d71fa591b74256ab249557831ad35
Henrik Aarnio <hjaarnio@gmail.com>
2017-08-20 22:34:58 +0300

    render track text

--- src/app/components/Track.js
+++ src/app/components/Track.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid } from "react-flexbox-grid";
+import { Grid, Row, Col } from "react-flexbox-grid";
 import { connect } from 'react-redux';
 import { Redirect } from "react-router-dom";
 import PropTypes from 'prop-types';
@@ -16,11 +16,25 @@ class Track extends Component {
     if(this.props.tracks.length > 0){
       var track = this.props.tracks.filter(track => track.slug === match.params.track);
       if(track.length === 0) return <Redirect to="/"/>;
+      else track = track[0];
     } else track = {};
+
+    var style = {
+      background: `url(${track.image})`
+    };
 
     return (
       <Grid fluid>
-        {match.params && match.params.track}
+        <Row center="xs sm md" style={style} className="track_header">
+          <Col className="track_name" xs={12} sm={12} md={12}>
+            {track.title}
+          </Col>
+        </Row>
+        <Row>
+          <Col className="track_content" xs={12} sm={12} md={12}>
+            <div dangerouslySetInnerHTML={{__html: track.content}}></div>
+          </Col>
+        </Row>
       </Grid>
     );
   }
