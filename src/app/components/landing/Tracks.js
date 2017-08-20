02b425433b34f3ad5797d5b50d6ffdc648f1e0bc
Tomi Hiltunen <tomi@innovaniac.com>
2017-08-20 14:16:03 +0300

    fix

--- src/app/components/landing/Tracks.js
+++ src/app/components/landing/Tracks.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { Grid, Row, Col } from "react-flexbox-grid";
 import "./landing.scss";
 import TrackElement from "../common/TrackElement";
-import { connect } from 'react-redux';
 
 class Tracks extends Component {
   componentWillMount() {
     // the first time we load the app, we need that tracks list
-    this.props.dispatch({ type: 'GET_TRACKS' });
+    this.props.dispatch({ type: "GET_TRACKS" });
   }
   render() {
-    var grid = [
-      [6, 6],
-      [6, 6],
-      [6, 6]
-    ];
+    var grid = [[6, 6], [6, 6], [6, 6]];
     var i = 0;
 
     return (
@@ -35,20 +31,24 @@ class Tracks extends Component {
             </p>
           </Col>
         </Row>
-          {grid.map((row) => {
-            return <Row height={1} center="xs">
+        {grid.map(row => {
+          return (
+            <Row height={1} center="xs">
               {row.map(width => {
-                var element = <Col xs={width}>
-                  {this.props.tracks[i] ?
-                    <TrackElement key={i} post={this.props.tracks[i]}></TrackElement> : <div></div>
-                  }
-                </Col>;
+                var element = (
+                  <Col xs={width}>
+                    {this.props.tracks[i]
+                      ? <TrackElement key={i} post={this.props.tracks[i]} />
+                      : <div />}
+                  </Col>
+                );
                 i += 1;
                 return element;
               })}
-            </Row>;
-          })}
-          <Row>
+            </Row>
+          );
+        })}
+        <Row>
           <Col xs>
             <Row center="xs">
               <Col xs={3}>
@@ -124,7 +124,7 @@ Tracks.propTypes = {
 
 // export the connected class
 function mapStateToProps(state) {
-  console.log('tracks', state.tracks);
+  console.log("tracks", state.tracks);
   return {
     tracks: state.tracks || [],
   };
