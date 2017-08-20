084ddb428b166be57370d55fe67f2a82b064198f
Angelos <angelos.arnis@gmail.com>
2017-08-20 22:31:22 +0300

    Footer stuff

--- src/app/components/landing/Footer.js
+++ src/app/components/landing/Footer.js
@@ -7,12 +7,28 @@ const Footer = () =>
     <Row start="xs" className="junction_footer">
       <Col xsOffset={1} lg={2}>
         <h2>Event</h2>
+        <p>Contact:<br />
+          hello@hackjunction.com<br />
+          partnerships@hackjunction.com
+        </p>
       </Col>
       <Col xsOffset={1} lg={2}>
         <h2>For Media</h2>
+        <p>
+          We are excited too, but content is coming soon. Promise!
+        </p>
       </Col>
       <Col xsOffset={1} lg={2}>
         <h2>Social</h2>
+        <p><a href="https://www.facebook.com/hackjunction">
+          <img src="https://cdn0.iconfinder.com/data/icons/social-network-7/50/3-128.png" width="24px;" />
+        Facebook</a></p>
+        <p><a href="https://www.instagram.com/hackjunction/">
+          <img src="http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c521.png" width="24px;" />
+        Instagram</a></p>
+        <p><a href="https://blog.hackjunction.com/">
+          <img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Medium-128.png" width="24px;" />
+        Medium</a></p>
       </Col>
       <Col xsOffset={1} lg={2}>
         <h2>Community</h2>
