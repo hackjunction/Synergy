f8ad66e527696f7a2daa00949bb95c10a5d9ecf2
loicmasson2 <loic@yousician.com>
2017-08-20 18:24:11 +0300

    add dot navigation, miss some animations and detect the cahnge of section on scroll

--- src/app/components/Landing.js
+++ src/app/components/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import './landing/landing.scss';
 import Welcome from './landing/Welcome';
 import About from './landing/About';
@@ -9,16 +10,37 @@ import Media from './landing/Media';
 import Pictures from './landing/Pictures';
 import Footer from './landing/Footer';
 
-const Landing = () =>
-  <div>
-    <Welcome />
-    <About />
-    <Tracks />
-    <Partners />
-    <Hacktalks />
-    <Media />
-    <Pictures />
-    <Footer />
-  </div>;
+class Landing extends React.Component {
+  render() {
+    return (
+      <div>
+        <div id="welcome">
+          <Welcome />
+        </div>
+        <div id="about">
+          <About />
+        </div>
+        <div id="tracks">
+          <Tracks />
+        </div>
+        <div id="partners">
+          <Partners />
+        </div>
+        <div id="hacktalks">
+          <Hacktalks />
+        </div>
+        <div id="media">
+          <Media />
+        </div>
+        <div id="pictures">
+          <Pictures />
+        </div>
+        <div id="footer">
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Landing;
