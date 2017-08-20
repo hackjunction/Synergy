6cb837021d48cd6b3d6c558e0cf0d117afb915dd
Tomi Hiltunen <tomi@innovaniac.com>
2017-08-20 14:56:06 +0300

    fix body styles

--- src/app/components/Landing.js
+++ src/app/components/Landing.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import './landing/landing.scss';
-import Welcome from './landing/Welcome';
-import About from './landing/About';
-import Tracks from './landing/Tracks';
-import Partners from './landing/Partners';
-import Hacktalks from './landing/Hacktalks';
-import Media from './landing/Media';
-import Pictures from './landing/Pictures';
-import Footer from './landing/Footer';
+import React from "react";
+import { Grid } from "react-flexbox-grid";
+import "./landing/landing.scss";
+import Welcome from "./landing/Welcome";
+import About from "./landing/About";
+import Tracks from "./landing/Tracks";
+import Partners from "./landing/Partners";
+import Hacktalks from "./landing/Hacktalks";
+import Media from "./landing/Media";
+import Pictures from "./landing/Pictures";
+import Footer from "./landing/Footer";
 
 const Landing = () =>
-  <div>
+  <Grid fluid>
     <Welcome />
     <About />
     <Tracks />
@@ -19,6 +20,6 @@ const Landing = () =>
     <Media />
     <Pictures />
     <Footer />
-  </div>;
+  </Grid>;
 
 export default Landing;
