9050dcad462037471083ad381126bddad762d58f
Ujjwal <ujjwal.mairh@gmail.com>
2017-08-21 13:30:05 +0300

    footer style

--- src/app/components/landing/Footer.js
+++ src/app/components/landing/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, Row, Col } from "react-flexbox-grid";
+import styles from "./Footer.c.scss";
 
 const Footer = () =>
   <Grid fluid>
@@ -20,13 +21,13 @@ const Footer = () =>
       <Col xsOffset={1} lg={3}>
         <h2>Social</h2>
         <p><a href="https://www.facebook.com/hackjunction">
-          <img src="https://cdn0.iconfinder.com/data/icons/social-network-7/50/3-128.png" width="24px;" />
+          <img className={styles.junction_footer_social_image} src="https://cdn0.iconfinder.com/data/icons/social-network-7/50/3-128.png" width="24px;" />
         Facebook</a></p>
         <p><a href="https://www.instagram.com/hackjunction/">
-          <img src="http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c521.png" width="24px;" />
+          <img className={styles.junction_footer_social_image} src="http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c521.png" width="24px;" />
         Instagram</a></p>
         <p><a href="https://blog.hackjunction.com/">
-          <img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Medium-128.png" width="24px;" />
+          <img className={styles.junction_footer_social_image} src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Medium-128.png" width="24px;" />
         Medium</a></p>
       </Col>
     </Row>
