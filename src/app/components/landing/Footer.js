68df928f37197e2d3c0a8e1834c86828070d1f15
Ujjwal <ujjwal.mairh@gmail.com>
2017-08-23 14:49:04 +0300

    Fix footer

--- src/app/components/landing/Footer.js
+++ src/app/components/landing/Footer.js
@@ -1,39 +1,40 @@
 import React from "react";
-import { Grid, Row, Col } from "react-flexbox-grid";
+import { Row, Col } from "react-flexbox-grid";
+import Block from '../viewBlocks/Block';
 import styles from "./Footer.c.scss";
 
 const Footer = () =>
-  <Grid fluid>
-    <Row start="xs" className="junction_footer">
-      <Col xsOffset={1} xs={12} sm={12} md={3} lg={3}>
+  <Block className={styles.block} contentClassName={styles.blockContent}>
+    <Row>
+      <Col xs={12} sm={3}>
         <h2>Event</h2>
         <p>Contact:<br />
           hello@hackjunction.com<br />
           partnerships@hackjunction.com
         </p>
       </Col>
-      <Col xsOffset={1} xs={12} sm={12} md={3} lg={3}>
+      <Col smOffset={1} xs={12} sm={3}>
         <h2>For Media</h2>
         <p>
           We are excited too, but content is coming soon. Promise!
         </p>
       </Col>
-      <Col xsOffset={1} xs={12} sm={12} md={3} lg={3}>
+      <Col smOffset={1} xs={12} sm={3}>
         <h2>Social</h2>
         <p><a href="https://www.facebook.com/hackjunction">
           <img className={styles.junction_footer_social_image} src="https://cdn0.iconfinder.com/data/icons/social-network-7/50/3-128.png" width="24px;" />
-        Facebook</a></p>
+          Facebook</a></p>
         <p><a href="https://www.instagram.com/hackjunction/">
           <img className={styles.junction_footer_social_image} src="http://www.stickpng.com/assets/images/580b57fcd9996e24bc43c521.png" width="24px;" />
-        Instagram</a></p>
+          Instagram</a></p>
         <p><a href="https://blog.hackjunction.com/">
           <img className={styles.junction_footer_social_image} src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Medium-128.png" width="24px;" />
-        Medium</a></p>
+          Medium</a></p>
       </Col>
     </Row>
-    <Row center="xs" className="subfooter">
+    <Row center="xs">
       <p>Designed and developed by the amazing Junction Team</p>
     </Row>
-  </Grid>;
+  </Block>;
 
 export default Footer;
