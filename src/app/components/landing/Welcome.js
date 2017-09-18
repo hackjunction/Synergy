f27c74c576d0e52de8f0044e286732c5124a7420
Angelos <angelos.arnis@gmail.com>
2017-09-18 14:31:19 +0300

    mlh badge

--- src/app/components/landing/Welcome.js
+++ src/app/components/landing/Welcome.js
@@ -17,6 +17,10 @@ const Welcome = () => (
               src="https://staging.hackjunction.com/wp-content/uploads/2017/08/junction_logo-1.png"
             />
           </Link>
+          <a className={styles.mlh} href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray">
+            <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg" alt="Major League Hacking 2017 Hackathon Season" />
+          </a>
+
         </Col>
       </Row>
       <Row center="xs" className="junction_welcome">
