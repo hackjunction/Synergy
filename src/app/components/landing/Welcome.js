6fef623a5ab007a7f39861a3155cb137e1736817
loicmasson2 <loic@yousician.com>
2017-09-21 08:40:41 +0300

    make the top nav take all the size

--- src/app/components/landing/Welcome.js
+++ src/app/components/landing/Welcome.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-flexbox-grid';
 import Scroll from 'react-scroll';
 import ReactRotatingText from 'react-rotating-text';
@@ -8,22 +7,22 @@ import Block from '../viewBlocks/Block';
 import TopNav from '../TopNav';
 
 const Welcome = () => (
-  <Scroll.Element name="welcome">
-    <Block className={styles.block} contentClassName={styles.blockContent}>
-      <TopNav />
-      <Row center="xs">
-        <Col>
-          <a
-            className={styles.mlh}
-            href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray"
-          >
-            <img
-              src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg"
-              alt="Major League Hacking 2017 Hackathon Season"
-            />
-          </a>
-        </Col>
-      </Row>
+  <Scroll.Element name="welcome" className={styles.block}>
+    <TopNav />
+    <Row center="xs">
+      <Col>
+        <a
+          className={styles.mlh}
+          href="https://mlh.io/seasons/na-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray"
+        >
+          <img
+            src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg"
+            alt="Major League Hacking 2017 Hackathon Season"
+          />
+        </a>
+      </Col>
+    </Row>
+    <Block contentClassName={styles.blockContent}>
       <Row center="xs" className="junction_welcome">
         <Col xs={12} className={styles.junction_hackthefuture}>
           <h1>
