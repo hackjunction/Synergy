a69128fd8bc00e0a2b4d4c2c5a43fbf65b81d72d
Angelos <angelos.arnis@gmail.com>
2017-09-28 14:56:02 +0300

    adding nitor logo

--- src/app/components/landing/Partners.js
+++ src/app/components/landing/Partners.js
@@ -115,6 +115,12 @@ const Partners = () =>
           </Col>
           <Col xs={4} md={3}>
             <img
+              src="https://staging.hackjunction.com/wp-content/uploads/2017/09/nitor.png"
+              className="responsive"
+            />
+          </Col>
+          <Col xs={4} md={3}>
+            <img
               src="https://staging.hackjunction.com/wp-content/uploads/2017/08/kone.png"
               className="responsive"
             />
