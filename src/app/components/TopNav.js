36c601947aebed78d560e2ef96e14ef5b1d9f3d3
Tomi Hiltunen <tomi@innovaniac.com>
2017-10-01 14:28:17 +0300

    prettify

--- src/app/components/TopNav.js
+++ src/app/components/TopNav.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styles from './TopNav.c.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./TopNav.c.scss";
 
 class TopNav extends React.Component {
   render() {
@@ -15,18 +15,34 @@ class TopNav extends React.Component {
           </Link>
           <nav className={styles.site_nav}>
             <ul center="xs">
-              <li><a href="/">HOME</a></li>
-              <li><a href="/junction2017">JUNCTION2017</a></li>
-              <li><a href="/all-tracks">TRACKS</a></li>
-              <li><a href="/partners">PARTNERS</a></li>
-              <li><a href="https://register.hackjunction.com/">APPLY</a></li>
+              <li>
+                <a href="/">HOME</a>
+              </li>
+              <li>
+                <a href="/junction2017">JUNCTION2017</a>
+              </li>
+              <li>
+                <a href="/all-tracks">TRACKS</a>
+              </li>
+              <li>
+                <a href="/partners">PARTNERS</a>
+              </li>
+              <li>
+                <a href="https://register.hackjunction.com/">APPLY</a>
+              </li>
             </ul>
           </nav>
-          <a className={styles.mlh} href="https://mlh.io/seasons/eu-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray">
-            <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg" alt="Major League Hacking 2017 Hackathon Season" />
+          <a
+            className={styles.mlh}
+            href="https://mlh.io/seasons/eu-2018/events?utm_source=na-2018&utm_medium=TrustBadge&utm_campaign=na-2018&utm_content=gray"
+          >
+            <img
+              src="https://s3.amazonaws.com/logged-assets/trust-badge/2018/gray.svg"
+              alt="Major League Hacking 2017 Hackathon Season"
+            />
           </a>
-      </header>
-    </div>
+        </header>
+      </div>
     );
   }
 }
