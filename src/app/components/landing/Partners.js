1049fe5a01dc511e4b54f54e8dbd0b26c9e4e1e1
Angelos <angelos.arnis@gmail.com>
2017-10-18 07:37:47 +0300

    spotify

--- src/app/components/landing/Partners.js
+++ src/app/components/landing/Partners.js
@@ -67,6 +67,12 @@ const Partners = () =>
         </Col>
         <Col xs={4} md={3}>
           <img
+            src="https://staging.hackjunction.com/wp-content/uploads/2017/10/spotify.png"
+            className="responsive"
+          />
+        </Col>
+        <Col xs={4} md={3}>
+          <img
             src="https://staging.hackjunction.com/wp-content/uploads/2017/08/suunto.png"
             className="responsive"
           />
