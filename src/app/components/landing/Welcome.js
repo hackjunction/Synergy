23cac726daf8075024c1dcb63ef63acf9ae9d6a0
Tomi Hiltunen <tomi@innovaniac.com>
2017-10-31 19:09:37 +0200

    add dark video overlay

--- src/app/components/landing/Welcome.js
+++ src/app/components/landing/Welcome.js
@@ -1,18 +1,32 @@
-import React from 'react';
-import { Row, Col } from 'react-flexbox-grid';
-import Scroll from 'react-scroll';
-import ReactRotatingText from 'react-rotating-text';
-import styles from './Welcome.c.scss';
-import Block from '../viewBlocks/Block';
-import TopNav from '../TopNav';
+import React from "react";
+import { Row, Col } from "react-flexbox-grid";
+import Scroll from "react-scroll";
+import ReactRotatingText from "react-rotating-text";
+import styles from "./Welcome.c.scss";
+import Block from "../viewBlocks/Block";
+import TopNav from "../TopNav";
 
 const Welcome = () => (
   <Scroll.Element name="welcome" className={styles.block}>
     <TopNav />
-    <video autoPlay loop muted playsInline poster="https://staging.hackjunction.com/wp-content/uploads/2017/08/front.jpg" className={styles.video}>
-      <source src="https://staging.hackjunction.com/wp-content/uploads/2017/08/vidmp4.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
-      <source src="https://staging.hackjunction.com/wp-content/uploads/2017/08/vidwebm.webm" type='video/webm; codecs="vp8, vorbis"' />
+    <video
+      autoPlay
+      loop
+      muted
+      playsInline
+      poster="https://staging.hackjunction.com/wp-content/uploads/2017/08/front.jpg"
+      className={styles.video}
+    >
+      <source
+        src="https://staging.hackjunction.com/wp-content/uploads/2017/08/vidmp4.mp4"
+        type="video/mp4; codecs=&quot;avc1.42E01E, mp4a.40.2&quot;"
+      />
+      <source
+        src="https://staging.hackjunction.com/wp-content/uploads/2017/08/vidwebm.webm"
+        type="video/webm; codecs=&quot;vp8, vorbis&quot;"
+      />
     </video>
+    <div className={styles.videoOverlay} />
     <Block contentClassName={styles.blockContent}>
       <Row center="xs" className="junction_welcome">
         <Col xs={12} className={styles.junction_hackthefuture}>
@@ -23,10 +37,10 @@ const Welcome = () => (
               <ReactRotatingText
                 color="#35E2DF"
                 items={[
-                  '1300 HACKERS',
-                  '80 NATIONALITIES',
-                  '60+ PARTNER COMPANIES',
-                  '20.000€ MAIN PRIZE',
+                  "1300 HACKERS",
+                  "80 NATIONALITIES",
+                  "60+ PARTNER COMPANIES",
+                  "20.000€ MAIN PRIZE",
                 ]}
               />
             </div>
