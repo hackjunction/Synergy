efbda704d6e16c75468e54a1df9e0fb755bfec64
Henrik Aarnio <hjaarnio@gmail.com>
2017-11-20 00:00:03 +0200

    forgot file

---
+++ src/app/components/TrackChallenges.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Challenge from './common/ChallengeElement';
+
+class TrackChallenges extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  componentWillMount() {
+    var track = this.props.track;
+    if(track.challenges){
+      track.challenges.forEach(challenge => {
+        if(!this.props.challenges[challenge.id]){
+          this.props.getChallenge(challenge.id);
+        }
+      });
+    }
+  }
+
+  render() {
+    var track = this.props.track;
+
+    return (
+      <div>
+        {track.challenges &&
+          track.challenges.sort((challenge1, challenge2) => {
+            if(this.props.challenges[challenge1.id] && this.props.challenges[challenge2.id])
+              return Number(this.props.challenges[challenge2.id].challenge_type) -
+                     Number(this.props.challenges[challenge1.id].challenge_type);
+            else return 0;
+          }).map(challenge => {
+            return (
+              <Challenge
+                key={challenge.id}
+                slug={challenge.slug}
+                title={challenge.title}
+                content={challenge.content}
+                post_id={challenge.id}
+              />
+            );
+          })}
+      </div>
+    );
+  }
+}
+
+TrackChallenges.propTypes = {
+  params: PropTypes.object,
+  track: PropTypes.object,
+  challenges: PropTypes.array,
+  getChallenge: PropTypes.func,
+  match: PropTypes.object,
+};
+
+function mapStateToProps(state) {
+  //console.log('tracks', state.tracks);
+  return {
+    tracks: state.tracks || [],
+    challenges: state.challenges
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    getChallenge(id) {
+      dispatch({ type: 'GET_CHALLENGE', id });
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TrackChallenges);
