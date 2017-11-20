9758e2287499a240f5150dcc6dcfa82b0a3dfbd3
Angelos <angelos.arnis@gmail.com>
2017-11-20 21:02:20 +0200

    track addition

--- src/app/components/AllTracksTrack.js
+++ src/app/components/AllTracksTrack.js
@@ -49,6 +49,7 @@ class AllTracksTrack extends Component {
                 );
               })}
               <Col xs={12} md={12}>
+                <a href={`tracks/${track.slug}`}><span className={styles.text_highlight}>SHOW ALL {track.title} CHALLENGES</span></a>
                 <hr className={styles.separator}></hr>
               </Col>
           </Row>
