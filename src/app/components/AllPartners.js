7c139c81f80c38bc1415305811ba7f0b84f41e46
Angelos <angelos.arnis@gmail.com>
2017-11-24 10:13:44 +0200

    mlh logo on website :P

--- src/app/components/AllPartners.js
+++ src/app/components/AllPartners.js
@@ -75,6 +75,12 @@ const AllPartners = () =>
         </Col>
         <Col xs={4} md={3}>
           <img
+            src="https://staging.hackjunction.com/wp-content/uploads/2017/11/mlh.png"
+            className="responsive"
+          />
+        </Col>
+        <Col xs={4} md={3}>
+          <img
             src="https://staging.hackjunction.com/wp-content/uploads/2017/10/spotify.png"
             className="responsive"
           />
