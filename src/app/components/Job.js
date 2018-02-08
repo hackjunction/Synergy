0c900523059a21899ceea517426c9902e2ebaf7b
Leo Drosdek <drosdek.leo@gmail.com>
2018-02-08 15:21:19 +0200

    Add proper styling and fetch all relevant content

--- src/app/components/Job.js
+++ src/app/components/Job.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import { Row, Col } from 'react-flexbox-grid';
+import { Row } from 'react-flexbox-grid';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 import styles from './Job.c.scss';
 
 function Job(props) {
   return (
     <Row className={styles.job_style}>
-      <Col xs={12} sm={12} md={12}>
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-      </Col>
+      <div className={styles.left}>
+        <img src={props.job.partner_logo} alt="Partner logo" />
+        <div className={styles.intermediate_text}>is looking for a</div>
+        <div className={styles.job_title}>{props.job.title}</div>
+        <hr className={styles.dividing_rule} />
+        <div className={styles.deadline}>
+          <div className={styles.intermediate_text}>
+            <span>APPLICATION ENDS</span>
+          </div>
+          <div className={styles.application_end_date}>
+            <span className={styles.application_end_date}>{moment(props.job.application_end_date).format('ll')}</span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.skill_heading}>SKILLS</div>
+        <div className={styles.skills}>{props.job.skills}</div>
+        {props.job.description_paragraphs.map(paragraph => <p>{paragraph}</p>)}
+      </div>
     </Row>
   );
 }
 
 Job.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired
+  job: PropTypes.object.isRequired
 };
 
 export default Job;
