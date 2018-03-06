ae4aff9458fdc1ecc9279d0ddc5e80ea02a161d5
Henrik Aarnio <hjaarnio@gmail.com>
2018-03-06 20:29:53 +0200

    edit junctionx page

--- src/app/components/JunctionX.js
+++ src/app/components/JunctionX.js
@@ -13,7 +13,7 @@ const Community = () => (
       <TitleMod
         title="What is JUNCTIONx?"
         content={[
-          'JUNCTIONx is the low-barrier entrypoint on your path to creating your own hackathon. We want to provide you with the necessary tools, guidelines, and especially our brand to make everything as easy as possible for you. This way you can concentrate on making your own Junction unique for participants, partners and especially for your organising team.',
+          'Since we have an outstanding understanding on organizing hackathons, JUNCTIONx-program was created to spread our knowledge further. We want to make it as easy as possible for aspiring hackathon organizers to kickstart their events and avoid the beginner’s pitfalls. JUNCTIONx is a program where we give our knowledge and the brand to organizers. We really want to empower people to solve the world’s issues with the help of the latest technologies.',
           'Want to hear more? Shoot us a message at global@hackjunction.com!'
         ]}
       />
@@ -24,13 +24,14 @@ const Community = () => (
           <h1>WHAT JUNCTIONx OFFERS?</h1>
           <h4>1. Organizers handbook</h4>
           <p>
-            Concludes our insider knowledge on organizing a Hackathon. It contains information about everything from
+            Concludes our insider knowledge on organizing a hackathon. It contains information about everything from
             setting up the venue to creating your team.
           </p>
           <h4>2. Use of Brand</h4>
           <p>
-            We have built our brand to a point where it is one of the most recognised hackathons in the world. The brand
-            will ease your job in many ways, for example in getting partners and applicants.
+            We have built our brand to a point where Junction is one of the most recognised hackathons in the world. The
+            JUNCTIONx brand will ease your job in many ways, such as reaching applicants and partners from our global
+            community.
           </p>
         </Col>
         <Col>
