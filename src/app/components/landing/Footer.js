52ee7f48b0649476cbedd33f7e62efdc78335872
Henrik Aarnio <hjaarnio@gmail.com>
2018-06-25 20:02:40 +0300

    update footer

--- src/app/components/landing/Footer.js
+++ src/app/components/landing/Footer.js
@@ -5,63 +5,65 @@ import styles from './Footer.c.scss';
 
 const Footer = () => (
   <Block className={styles.block} contentClassName={styles.blockContent}>
-    <Row>
-      <Col xs={12} sm={3}>
-        <h2>Event</h2>
-        <p>
-          Contact:<br />
-          hello@hackjunction.com<br />
-          partnerships@hackjunction.com
-        </p>
+    <Row center="xs">
+      <Col sm={4}>
+        <h2>CONTACT</h2>
+        <p className={styles.footer_email}>hello@hackjunction.com</p>
+        <p className={styles.footer_email}>partnerships@hackjunction.com</p>
       </Col>
-      <Col smOffset={1} xs={12} sm={3}>
-        <h2>For Media</h2>
-        <p>
-          <a href="https://www.flickr.com/people/151708924@N07/" className={styles.footer_link}>
-            Official Junction photos
-          </a>
-        </p>
-        <p>
-          <a href="https://drive.google.com/open?id=0B2AGGlwAtVYLYTdsSE9tZGtoTE0" className={styles.footer_link}>
-            Presskit
-          </a>
-        </p>
+      <Col sm={4}>
+        <h2>FOR MEDIA</h2>
+        <a href="https://www.flickr.com/people/151708924@N07/" className={styles.footer_button}>
+          OFFICIAL PHOTOS
+        </a>
       </Col>
-      <Col smOffset={1} xs={12} sm={3}>
-        <h2>Social</h2>
-        <p>
-          <a href="https://www.facebook.com/hackjunction">
-            <img
-              className={styles.junction_footer_social_image}
-              src="https://staging.hackjunction.com/wp-content/uploads/2017/08/fb.png"
-              alt=""
-            />
-            Facebook
-          </a>
-        </p>
-        <p>
-          <a href="https://www.instagram.com/hackjunction/">
-            <img
-              className={styles.junction_footer_social_image}
-              src="https://staging.hackjunction.com/wp-content/uploads/2017/08/ig.png"
-              alt=""
-            />
-            Instagram
-          </a>
-        </p>
-        <p>
-          <a href="https://blog.hackjunction.com/">
-            <img
-              className={styles.junction_footer_social_image}
-              src="https://staging.hackjunction.com/wp-content/uploads/2017/08/md.png"
-              alt=""
-            />
-            Medium
-          </a>
-        </p>
+      <Col sm={4}>
+        <h2>SOCIAL</h2>
+        <Row center="xs">
+          <Col>
+            <p>
+              <a href="https://www.instagram.com/hackjunction/">
+                <img
+                  className={styles.footer_social_image}
+                  src="https://staging.hackjunction.com/wp-content/uploads/2017/08/ig.png"
+                  alt="Instagram"
+                />
+              </a>
+            </p>
+            <p>
+              <a href="https://blog.hackjunction.com/">
+                <img
+                  className={styles.footer_social_image}
+                  src="https://staging.hackjunction.com/wp-content/uploads/2017/08/md.png"
+                  alt="Medium"
+                />
+              </a>
+            </p>
+          </Col>
+          <Col>
+            <p>
+              <a href="https://www.facebook.com/hackjunction">
+                <img
+                  className={styles.footer_social_image}
+                  src="https://staging.hackjunction.com/wp-content/uploads/2017/08/fb.png"
+                  alt="Facebook"
+                />
+              </a>
+            </p>
+            <p>
+              <a href="https://www.twitter.com/hackjunction">
+                <img
+                  className={styles.footer_social_image}
+                  src="https://staging.hackjunction.com/wp-content/uploads/2017/08/fb.png"
+                  alt="Twitter"
+                />
+              </a>
+            </p>
+          </Col>
+        </Row>
       </Col>
     </Row>
-    <Row center="xs" className={styles.base}>
+    {/*    <Row center="xs" className={styles.base}>
       <p>
         <a href="https://staging.hackjunction.com/wp-content/uploads/2018/05/Junction-Oy-Privacy-Policy-24052018.pdf">
           Privacy Policy
@@ -71,12 +73,12 @@ const Footer = () => (
           Terms of Service
         </a>
       </p>
-    </Row>
-    <Row center="xs">
+    </Row> */}
+    <Row center="xs" className={styles.base}>
       <p>
         Designed and developed with 💕 &amp; ☕ by the amazing Junction Team
-        <br />
-        Released as <a href="https://github.com/hackjunction/Synergy">Open Source project under the MIT License</a>.
+        {/*        <br />
+        Released as <a href="https://github.com/hackjunction/Synergy">Open Source project under the MIT License</a>. */}
       </p>
     </Row>
   </Block>
