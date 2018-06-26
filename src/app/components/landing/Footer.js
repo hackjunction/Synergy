abb7f32ab0ea1f2fcd60b6357e2e51d175dba9c3
Henrik Aarnio <hjaarnio@gmail.com>
2018-06-26 23:10:43 +0300

    adjust footer logos

--- src/app/components/landing/Footer.js
+++ src/app/components/landing/Footer.js
@@ -6,59 +6,57 @@ import styles from './Footer.c.scss';
 const Footer = () => (
   <Block className={styles.block} contentClassName={styles.blockContent}>
     <Row center="xs">
-      <Col sm={4}>
+      <Col xs={4}>
         <h2>CONTACT</h2>
         <p className={styles.footer_email}>hello@hackjunction.com</p>
         <p className={styles.footer_email}>partnerships@hackjunction.com</p>
       </Col>
-      <Col sm={4}>
+      <Col xs={4}>
         <h2>FOR MEDIA</h2>
         <a href="https://www.flickr.com/people/151708924@N07/" className={styles.footer_button}>
           OFFICIAL PHOTOS
         </a>
       </Col>
-      <Col sm={4}>
+      <Col xs={4}>
         <h2>SOCIAL</h2>
         <Row center="xs">
-          <Col>
-            <p>
-              <a href="https://www.instagram.com/hackjunction/">
-                <img
-                  className={styles.footer_social_image}
-                  src="https://staging.hackjunction.com/wp-content/uploads/2017/08/ig.png"
-                  alt="Instagram"
-                />
-              </a>
-            </p>
-            <p>
-              <a href="https://blog.hackjunction.com/">
-                <img
-                  className={styles.footer_social_image}
-                  src="https://staging.hackjunction.com/wp-content/uploads/2017/08/md.png"
-                  alt="Medium"
-                />
-              </a>
-            </p>
+          <Col xs={4}>
+            <a href="https://www.instagram.com/hackjunction/">
+              <img
+                className={styles.footer_social_image}
+                src="https://staging.hackjunction.com/wp-content/uploads/2018/06/glyph-logo_May2016.svg"
+                alt="Instagram"
+              />
+            </a>
           </Col>
-          <Col>
-            <p>
-              <a href="https://www.facebook.com/hackjunction">
-                <img
-                  className={styles.footer_social_image}
-                  src="https://staging.hackjunction.com/wp-content/uploads/2017/08/fb.png"
-                  alt="Facebook"
-                />
-              </a>
-            </p>
-            <p>
-              <a href="https://www.twitter.com/hackjunction">
-                <img
-                  className={styles.footer_social_image}
-                  src="https://staging.hackjunction.com/wp-content/uploads/2017/08/fb.png"
-                  alt="Twitter"
-                />
-              </a>
-            </p>
+          <Col xs={4}>
+            <a href="https://blog.hackjunction.com/">
+              <img
+                className={styles.footer_social_image}
+                src="https://staging.hackjunction.com/wp-content/uploads/2018/06/Monogram.svg"
+                alt="Medium"
+              />
+            </a>
+          </Col>
+        </Row>
+        <Row center="xs">
+          <Col xs={4}>
+            <a href="https://www.facebook.com/hackjunction">
+              <img
+                className={styles.footer_social_image}
+                src="https://staging.hackjunction.com/wp-content/uploads/2018/06/flogo-HexRBG-Wht-58.svg"
+                alt="Facebook"
+              />
+            </a>
+          </Col>
+          <Col xs={4}>
+            <a href="https://www.twitter.com/hackjunction">
+              <img
+                className={styles.footer_social_image}
+                src="https://staging.hackjunction.com/wp-content/uploads/2018/06/Twitter_Logo_WhiteOnImage.svg"
+                alt="Twitter"
+              />
+            </a>
           </Col>
         </Row>
       </Col>
