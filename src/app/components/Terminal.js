74dcafc7266f0560de8e51ec5cc9ee4a51959d11
Markus Ihamuotila <markus.ihamuotila@aalto.fi>
2018-08-23 00:10:22 +0300

    Topnav working with slideshow, fixed typos in Terminal.

--- src/app/components/Terminal.js
+++ src/app/components/Terminal.js
@@ -24,11 +24,11 @@ const Terminal = () => (
         <Col xs={12} className={styles.parag}>
           <br />
           <br />
-          Start the Junction week unforgettable way in Terminal! Terminal provides magnificent possibility to passionate
-          young talents to make connections with exhilarating international companies that are looking for in example
-          developers and meet other hackers. A four day event includes extremely fun days, learning from each others,
-          having nights out in Helsinki and getting most out of the entire Junction week! Participants of Terminal also
-          have possibility to join the Keynotes part of Hack Talks.
+          Start the Junction week in an unforgettable way with Terminal! Terminal provides magnificent possibilities for
+          passionate young talents to make connections with exhilarating international companies that are looking for,
+          for example, developers, and meet other hackers. A four day event includes extremely fun days, learning from
+          each other, having nights out in Helsinki and getting the most out of the entire Junction week! Participants
+          of Terminal also have the possibility to join the Keynotes part of Hack Talks.
           <br />
         </Col>
       </Row>
@@ -39,8 +39,8 @@ const Terminal = () => (
         <Col xs={12} className={styles.parag}>
           <br />
           <br />
-          Participants of Terminal are chosen amongst of the participants of Junction. Apply to Terminal via the
-          Junction registration platform and click that you are interested to join Terminal and fill in your answers for
+          Participants of Terminal are chosen from amongst the participants of Junction. Apply to Terminal via the
+          Junction registration platform, click that you are interested to join Terminal and fill in your answers for
           the questions. Hope to see you in November!
           <br />
         </Col>
@@ -66,11 +66,11 @@ const Terminal = () => (
       <Row className="junction_terminal">
         <Col xs={12} className={styles.parag}>
           <br />
-          Terminal starts on the 19th of November with kick-off session and every day the specific partner company will
-          host the night. There are also partner-hosted events and events to explore Helsinki during the days. Wednesday
-          21st is the last day of having company hosted events and Thursday 22nd is allocated free so participants and
-          the partner companies can book one-on-one meetings in terms of recruiting and join Hack Talks Keynotes during
-          the evening! On the Friday 23rd we are getting ready for the Junction 2018! Accurate information about the
+          Terminal starts on the 19th of November with a kick-off session and every day the specific partner company
+          will host the night. There are also partner-hosted events and events to explore Helsinki during the days.
+          Wednesday 21st is the last day of company hosted events and Thursday 22nd is allocated free, so participants
+          and the partner companies can book one-on-one meetings in terms of recruiting and join Hack Talks Keynotes
+          during the evening! On Friday 23rd we are getting ready for Junction 2018! Accurate information about the
           schedule will be completed later.
           <br />
         </Col>
@@ -81,8 +81,8 @@ const Terminal = () => (
       <Row className="junction_terminal">
         <Col xs={12} className={styles.parag}>
           <br />
-          Welcome to Finland! Participating to Terminal means that we will provide you accommodation together with other
-          Terminal participants for the whole week! More information coming soon.
+          Welcome to Finland! Participating in Terminal means that we will provide you with accommodation together with
+          other Terminal participants for the whole week! More information coming soon.
           <br />
         </Col>
       </Row>
@@ -103,8 +103,8 @@ const Terminal = () => (
           young students or recently graduated ones.
           <br />
           <h3> How? </h3>
-          Partners organize events to introduce their company and thoughts for the participants. On Thursday there are
-          time to book one on one meetings with participants.
+          Partners organize events to introduce their company and thoughts for the participants. On Thursday 22nd there
+          is time to book one on one meetings with participants.
           <br />
           <h3> Additional info </h3>
           The amount of participants is approximately 120 persons and every partner will get one slot during day /
